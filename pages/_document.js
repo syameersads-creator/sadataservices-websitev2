c550b118295b0ffeb2714b79ab1012a42ecd435a
syameersads-creator <syameer.sads@gmail.com>
2025-10-12 22:53:22 +0800

    Favicon update
    Favicon tab image update

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,25 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+
+        {/* Meta tags */}
+        <meta name="theme-color" content="#0097E6" />
+        <meta
+          name="description"
+          content="S&A Data Services - Digital transformation partner for the construction and built environment sectors."
+        />
+        <meta property="og:title" content="S&A Data Services" />
+        <meta
+          property="og:description"
+          content="Your Project. Smarter. Faster. Digital."
+        />
+        <meta property="og:image" content="/favicon.ico" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
